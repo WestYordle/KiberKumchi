c17a61ca7d55666efcc5af0d185a6796478d21c5
N1ck227 <kartenoire2@gmail.com>
2025-05-03 15:58:38 +0300

    ﻿звуки кнопок в меню

--- menu/menu.gd
+++ menu/menu.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+@onready var menu_buttuns_click: AudioStreamPlayer = $menu_buttuns_click
+
 
 func _on_play_pressed() -> void:
+<<<<<<< HEAD
 	get_tree().change_scene_to_file("res://location/train.tscn")
+=======
+	menu_buttuns_click.play()
+	get_tree().change_scene_to_file("res://location/testlocation.tscn")
+>>>>>>> 95e9a85 (звуки кнопок в меню)
 
 
 func _on_exit_pressed() -> void:
+	menu_buttuns_click.play()
+	await menu_buttuns_click.finished
 	get_tree().quit()
 
 func _on_exit_mouse_entered() -> void:
