ba0bcf34a3ebfea3e8a95a9f96bc9646c6a56227
ЕгоркаПомидорка <152358656+WestYordle@users.noreply.github.com>
2025-05-03 23:41:41 +0500

    Фигня какая-то

--- dialog/dialog.gd
+++ dialog/dialog.gd
@@ -1,33 +1,31 @@
 extends CanvasLayer
 
 @export_file("*.json") var d_file
+# Called when the node enters the scene tree for the first time.
 var dialoge
 var json = JSON.new()
 var part_dialoge 
 var dialoge_reset : int
 var dialog_loag = false
 var npc
-var has_shown_dialogue = false # Новая переменная, отслеживающая показ диалога
 
 func _ready() -> void:
 	Signals.connect("dialoge", Callable(self, "_on_dialog"))
 	$".".visible = false
 
 func start():
-	if not has_shown_dialogue: # Запустится только если ещё не было показано
-		has_shown_dialogue = true # Ставим флаг, что диалог уже показан
-		part_dialoge = -1
-		dialoge = load_dialoge()
-
+	part_dialoge = -1
+	dialoge = load_dialoge()
+	
 func load_dialoge():
 	var file = FileAccess.open(d_file, FileAccess.READ)
 	file = file.get_as_text()
 	return JSON.parse_string(file)
-
+	
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("use") and dialog_loag == true:
 		next_chat()
-
+		
 func next_chat():
 	if part_dialoge == -1:
 		$".".visible = true
@@ -38,7 +36,7 @@ func next_chat():
 	else: 
 		$".".visible = false
 		start()
-
+		
 func _on_dialog(chat):
 	print(chat)
 	if chat != "out":
--- menu/menu.gd
+++ menu/menu.gd
@@ -1,14 +1,9 @@
 extends Node2D
 
-@onready var menu_buttuns_click: AudioStreamPlayer = $menu_buttuns_click
-
 
 func _on_play_pressed() -> void:
-	menu_buttuns_click.play()
 	get_tree().change_scene_to_file("res://location/testlocation.tscn")
 
 
 func _on_exit_pressed() -> void:
-	menu_buttuns_click.play()
-	await menu_buttuns_click.finished
 	get_tree().quit()
